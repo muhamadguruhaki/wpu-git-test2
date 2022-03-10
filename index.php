261ab58b4486a5f895af44ce0b7a2cd99c0d4f04
muhamadguruh15 <muhamadguruh15@gmail.com>
2022-03-10 14:15:33 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, nihil? Laborum accusamus quas aperiam cum inventore, sapiente, quae eaque deleniti architecto, maiores pariatur necessitatibus eum libero temporibus quia. Doloribus, ut?</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
 </body>
 </html>
